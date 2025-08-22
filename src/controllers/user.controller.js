d24dd88a88b17948ed38a74f213a66b5a46c4174
ShreshthaPandey <shreshthaP2624@gmail.com>
2025-08-22 16:24:55 +0530

    lec 21

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {apiError} from "../utils/apiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { apiResponse } from "../utils/apiResponse.js";
+import mongoose from "mongoose"
 import jwt from "jsonwebtoken"
  
 
@@ -18,7 +19,7 @@ const generateAccessAndRefreshToken = async (userId)=>
 
     return {accessToken , refreshToken}
 
-    }catch{
+    }catch(error){
         throw new apiError(500 , "something went wrong while generating refresh token")
     }
 }
@@ -319,6 +320,60 @@ const updateUserCoverImage =  asyncHandler(async(req, res)=>{
 
 
 })
+
+const getWatchHistory = asyncHandler(async(req , res) => {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField:"owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username: 1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                           owner:{
+                            $first: "$owner"
+                           }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+return res.status(200).json(
+    new apiResponse(
+        200,
+        user[0]?.watchHistory || [],
+        "watch history feetched successfully"
+    )
+);
+});
+
 export {
     registerUser,
      loginUser,
@@ -328,6 +383,7 @@ export {
      getCurrentUser,
      updateAccountDetails,
      updateUserAvatar,
-     updateUserCoverImage
+     updateUserCoverImage,
+     getWatchHistory
      
 }
